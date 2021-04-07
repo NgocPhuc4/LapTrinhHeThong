454776bfe7356eb58671d7b840fe4e476494c7a6
Lam Thanh Ngan <60861471+LamThanhNgan@users.noreply.github.com>
2021-04-07 18:28:19 +0700

    Update bai1.s

--- Lab2/bai1.s
+++ Lab2/bai1.s
@@ -7,12 +7,14 @@ output:
 .section .text
 	.globl _start
 _start:
-	movl $1, %edx
+	# Chuyen so sang ki tu, loai bo ki tu null
 	addl $48, output
 	subl $1, output
-	movl $output, %ecx
-	movl $4, %eax
-	movl $1, %ebx
-	int $0x80
-	movl $1, %eax
-	int $0x80
+	
+	movl $1, %edx                  # do dai chuoi duoc in ra
+	movl $output, %ecx             #chuoi duoc in ra
+	movl $4, %eax                  #sys_write
+	movl $1, %ebx                  # file descriptor stdout
+	int $0x80                      #call kernel
+	movl $1, %eax                  #sys_exit
+	int $0x80                      # call kernel
